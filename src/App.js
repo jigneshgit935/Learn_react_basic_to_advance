d6432e67f2f9a4b11b73fb6a0b6a38c8a0da760e
Jignesh <jignesh.sharma.707@gmail.com>
2023-06-26 18:26:56 +0530

    class-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import User from './User';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hello World</h1>
+        <User />
+      </div>
+    </>
   );
 }
 
