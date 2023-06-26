4038aeb7dc878593408d09188ff06d02ca1e02d8
Jignesh <jignesh.sharma.707@gmail.com>
2023-06-26 18:17:46 +0530

    react-startup-setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.css';
-import User from './User';
-// import { User } from './User';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <User />
-      <User />
-      <User />
-      <User />
     </div>
   );
 }
 
 export default App;
-
-// export function User() {
-//   return (
-//     <div>
-//       <h1>Functional Component</h1>
-//     </div>
-//   );
-// }
