6db13291f5f4cdf229efde341685c50a2daae90c
Jignesh <jignesh.sharma.707@gmail.com>
2023-06-26 18:39:14 +0530

    JSX-ReactJs

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import './App.css';
 import User from './User';
 
 function App() {
-  function Common() {
-    return <div>Component inside Component</div>;
-  }
-
   return (
     <>
       <div className="App">
         <h1>Hello World</h1>
         <User />
-        {Common()}
       </div>
     </>
   );
---
+++ src/User.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const User = () => {
+  return (
+    <>
+      <div>
+        <h1>User</h1>
+        {2 * 2}
+      </div>
+    </>
+  );
+};
+
+export default User;
