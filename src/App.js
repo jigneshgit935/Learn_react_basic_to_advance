03483ab0f9792e50bb4679da52142b3121f54548
Jignesh <jignesh.sharma.707@gmail.com>
2023-06-27 01:43:29 +0530

    class-component-props

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import UserCard from './UserCard';
-const App = () => {
-  const [name, setName] = useState('Jinu');
+import UserList from './UserList';
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Jignesh Sharma',
+    };
+  }
 
-  // const userData = [
-  //   {
-  //     name: 'jinu',
-  //     age: 22,
-  //     email: 'jinu@gmail.com',
-  //   },
-  //   {
-  //     name: 'Jignesh',
-  //     age: 12,
-  //     email: 'jignesh@gmail.com',
-  //   },
-  //   {
-  //     name: 'asur',
-  //     age: 54,
-  //     email: 'asur@gmail.com',
-  //   },
-  // ];
-  return (
-    <div className="App">
-      <h1>Props</h1>
-      <UserCard name={name} />
+  render() {
+    return (
+      <div className="App">
+        <h1>Class Component Props</h1>
+        {/* <UserList name="jinu" email="jinu@gmail.com" /> */}
 
-      <button onClick={() => setName('jignesh sharma')}> Click me</button>
-
-      {/* {userData.map((user, index) => (
-        <UserCard
-          key={index}
-          name={user.name}
-          age={user.age}
-          email={user.email}
-        />
-      ))} */}
-    </div>
-  );
-};
+        {/* Exmaple 2 */}
+        <UserList name={this.state.name} email="jinu@gmail.com" />
+        <button
+          onClick={() => this.setState({ name: 'Name is Sharma jignesh' })}
+        >
+          Update
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
