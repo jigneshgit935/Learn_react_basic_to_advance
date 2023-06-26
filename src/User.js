20cf556b73fc13ddad8a02c575b19ca2392a44f4
Jignesh <jignesh.sharma.707@gmail.com>
2023-06-26 18:10:51 +0530

    functional-component

---
+++ src/User.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const User = () => {
+  return (
+    <div>
+      <h1>Functional Component</h1>
+    </div>
+  );
+};
+
+export default User;
+
+// export function User() {
+//   return (
+//     <div>
+//       <h1>Functional Component</h1>
+//     </div>
+//   );
+// }
