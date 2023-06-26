e711f55bcfac1d460c3ff9ed1d4a455a091d40e2
Jignesh <jignesh.sharma.707@gmail.com>
2023-06-26 18:30:18 +0530

    component-inside-component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import User from './User';
 
 function App() {
+  function Common() {
+    return <div>Component inside Component</div>;
+  }
+
   return (
     <>
       <div className="App">
         <h1>Hello World</h1>
         <User />
+        {Common()}
       </div>
     </>
   );
