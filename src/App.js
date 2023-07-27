b6dd87cbc8a7ea0116cf794be94c93a66d5ca8dc
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-27 17:12:17 +0530

    form-handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
+import Form from './Form';
 const App = () => {
-  const [status, setStatus] = useState(false);
-
   return (
     <div className="App">
-      <h1>Hide and Toggle</h1>
-      {status ? <div className="status">Content</div> : ''}
+      <h1>Form and Handling</h1>
 
-      <button onClick={() => setStatus(false)}>hide</button>
-      <button onClick={() => setStatus(true)}>Show</button>
-
-      {/* Using only one button */}
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <button onClick={() => setStatus(!status)}>toggle</button>
+      <Form />
     </div>
   );
 };
