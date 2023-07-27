7928dd3bfbddec22cdddf90fc6fd8bc0d6eb2f03
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-28 00:20:28 +0530

    pass function as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
-import FormValidation from './FormVali';
+import User from './User';
 
 const App = () => {
+  // example 1
+  const buttonClick = () => {
+    console.log('Button Clicked');
+  };
+
+  // example2
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
-      <h1>Form Validation</h1>
-
-      <FormValidation />
+      <h1>Passing Function as Props</h1>
+      <User
+        buttonClick={buttonClick}
+        count={count}
+        incrementCount={incrementCount}
+      />
     </div>
   );
 };
