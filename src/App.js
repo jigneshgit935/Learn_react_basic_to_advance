d3ab3493aee4d8dc96aaa2a01efea8b05f63f894
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-28 00:44:55 +0530

    contructor method

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import User from './User';
 
-const App = () => {
-  // example 1
-  const buttonClick = () => {
-    console.log('Button Clicked');
-  };
-
-  // example2
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div className="App">
-      <h1>Passing Function as Props</h1>
-      <User
-        buttonClick={buttonClick}
-        count={count}
-        incrementCount={incrementCount}
-      />
-    </div>
-  );
-};
+class App extends Component {
+  // example1
+  constructor() {
+    super();
+    console.log('Contructor is called');
+  }
+  render() {
+    console.log('Render is called');
+    return (
+      <div className="App">
+        <h1>React LifeCycle</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
