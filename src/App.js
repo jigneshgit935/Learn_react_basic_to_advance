da65b9ecfc7e3b67984e311a28b7aa3685f5c4b0
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-28 14:45:11 +0530

    component-didUpdate

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+
 class App extends Component {
-  // example1
   constructor() {
     super();
+
     this.state = {
+      name: 'jingesh',
       count: 0,
     };
     console.log('Constructor called');
   }
-
-  // example2
-  incrementCount = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
-  };
-
   // example1
-  componentDidMount() {
-    console.log('Component DidMount called');
+
+  componentDidUpdate(prevProps, prevState) {
+    // example1
+    if ((this.state.count, prevState.count)) {
+      alert('working');
+      console.log('Component Did update', prevState.count, this.state.count);
+    }
   }
+
   render() {
-    console.log('Render');
+    console.log('render');
     return (
       <div className="App">
-        <h1>React Component DidMount</h1>
+        <h1>React Component Did Update</h1>
 
-        {/* example 2 */}
-        <h2>Count:{this.state.count}</h2>
-        <button onClick={this.incrementCount}> Increment</button>
+        {/* example2 */}
+        <h3>{this.state.name}</h3>
+
+        <button onClick={() => this.setState({ name: 'Jignesh Sharma' })}>
+          UpdateState
+        </button>
+
+        {/* example 3 */}
+        <h6>{this.state.count}</h6>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Upsate Button
+        </button>
       </div>
     );
   }
