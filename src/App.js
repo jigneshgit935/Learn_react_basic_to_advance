1902d4830d41bcc926b76a43b50b44c27a2d84a7
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-04 17:38:00 +0530

    useffect conditonal rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import UserTotal from './UserTotal';
 const App = () => {
   const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(100);
 
   useEffect(() => {
-    // alert('useEffect Hooks');
-    console.log('useEffect Hooks');
+    console.log('After Render!!');
+  }, [count]);
 
-    console.log('Updating');
-  });
   return (
     <div className="App">
-      <h1>useEffect</h1>
+      <h1>useEffect with conditon hooks</h1>
+
+      <UserTotal count={count} total={total} />
 
       <br />
       <br />
-      <h2>Count : {count}</h2>
+      <br />
+      {/* <h2>Count: {count}</h2> */}
       <button onClick={() => setCount(count + 1)}>Update Count</button>
+      <br />
+      <br />
+      <br />
+      {/* <h2>Total: {total}</h2> */}
+      <button onClick={() => setTotal(total + 1)}>Update Total</button>
     </div>
   );
 };
