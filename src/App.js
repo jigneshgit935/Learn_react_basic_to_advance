585237f9ea35d897123633e33250d9124d9c31f0
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-11 04:50:56 +0530

    array-map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import './App.css';
-import { Alert, Button } from 'react-bootstrap';
 const App = () => {
+  // example 1
+  const names = ['jinu', 'jignesh', 'rajesh', 'kamlesh'];
+
+  // example 2
+  const products = [
+    {
+      id: 1,
+      name: 'product 1',
+      price: 10,
+    },
+    {
+      id: 2,
+      name: 'product 2',
+      price: 20,
+    },
+    {
+      id: 3,
+      name: 'product 3',
+      price: 30,
+    },
+    {
+      id: 4,
+      name: 'product 4',
+      price: 40,
+    },
+  ];
+
+  // example 3 return map function
+  const fruits = ['Apple', 'Banana', 'Mango'];
+
+  // using map
+  const frutItems = fruits.map((fruit, index) => <li key={index}>{fruit}</li>);
   return (
-    <div className="App ">
-      <h1 className="">Bootstrap</h1>
-      <Button variant="primary">button</Button>
-      <Alert variant="danger">This is a alert—check it out!</Alert>s
+    <div className="App">
+      <h1>Array map</h1>
+      {/* example 1 */}
+      <ul>
+        {names.map((name, index) => {
+          return (
+            <li
+              key={index}
+              style={{ textTransform: 'capitalize', fontSize: '20px' }}
+            >
+              {name}
+            </li>
+          );
+        })}
+      </ul>
+
+      {/* example 2 */}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+
+      {/* exmaple 3 */}
+      <ul>{frutItems}</ul>
     </div>
   );
 };
