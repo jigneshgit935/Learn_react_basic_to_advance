634e105a636a280d5b48bbd5634bd55e0481931a
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-11 04:26:44 +0530

    style components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import UserTotal from './UserTotal';
+// examppl 2
+import './style.css';
+
+// example 4
+import Styles from './style.module.css';
+
+// example 5
+import './style.scss';
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [total, setTotal] = useState(100);
-
-  useEffect(() => {
-    console.log('After Render!!');
-  }, [count]);
-
+  // example 3
+  const headline = {
+    color: '#000',
+    backgroundColor: 'red',
+  };
   return (
     <div className="App">
-      <h1>useEffect with conditon hooks</h1>
+      <h1>React Style Methods</h1>
 
-      <UserTotal count={count} total={total} />
+      {/* example 1 {inline css} */}
+      <h1 style={{ color: 'white', backgroundColor: 'green' }}>Inline Css</h1>
 
-      <br />
-      <br />
-      <br />
-      {/* <h2>Count: {count}</h2> */}
-      <button onClick={() => setCount(count + 1)}>Update Count</button>
-      <br />
-      <br />
-      <br />
-      {/* <h2>Total: {total}</h2> */}
-      <button onClick={() => setTotal(total + 1)}>Update Total</button>
+      {/* example 2 Normal css */}
+      <h1 className="primary">Normal Css</h1>
+
+      {/* example 3 css in js */}
+      <h1 style={headline}> Css in js</h1>
+
+      {/* example 4 css module */}
+      <h1 className={Styles.title}> Css Module</h1>
+
+      {/* example 5 sass and scss */}
+      <h1 className="primary-sass">
+        Sass and <span>Scss</span>
+      </h1>
     </div>
   );
 };
