f1cf1f3ab3b66305fac90d40f2906606e5de1aeb
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-11 06:43:10 +0530

    Fragment react

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import './App.css';
-import Resusable from './Resusable';
-import ReusableList from './ReusableList';
 const App = () => {
-  const handleButton = () => {
-    alert('button clicked');
-  };
-  const handleButtonAgain = () => {
-    alert('Click again clicked');
-  };
-  const handleButtonSubmit = () => {
-    alert('Submit clicked');
-  };
-
-  // exampe 2
-  const countries = ['USA', 'India', 'Canada', 'Austrailia', 'SriLanka'];
   return (
-    <div className="App">
-      <h1>Resuable components</h1>
-      <Resusable text="Click Me" onClick={handleButton} />
-      <Resusable text="Click Again" onClick={handleButtonAgain} />
-      <Resusable text="Submit" onClick={handleButtonSubmit} />
-
-      <br />
-      <br />
-      <br />
-
-      <ReusableList items={countries} />
-    </div>
+    <>
+      <>
+        <h1>Fragment</h1>
+      </>
+    </>
   );
 };
 
--- src/Resusable.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Resusable = ({ text, onClick }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-
-export default Resusable;
