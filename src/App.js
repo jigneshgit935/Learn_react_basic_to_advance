c76280e117a64fa76694e95f96ca7ae54b0271c3
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-24 09:28:27 +0530

    react-fetch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import ChildComponent from './ChildComponent';
+import GetMethod from './GetMethod';
 const App = () => {
-  // example 1
-  const [count, setCount] = useState(0);
-
-  // examplw 1
-  console.log('parent rendered');
   return (
     <div className="App">
-      <h2>Memo</h2>
-
-      <ChildComponent count={count} />
-
-      <h2>Count : {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Count+</button>
+      <h2>React Fetch API</h2>
+      <div>
+        <GetMethod />
+      </div>
     </div>
   );
 };
